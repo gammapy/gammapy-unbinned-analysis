a094dd558e98d60638ec3eb0b9a1741228c73916
tunbehaun273 <tim.unbehaun@fau.de>
2023-02-03 12:11:20 +0100

    validated and implemented the make_acceptance function

--- unbinned-analysis/utils.py
+++ unbinned-analysis/utils.py
@@ -159,60 +159,147 @@ def make_exposure_factors(livetime, aeff, pointing, coords):
     exposure = aeff.evaluate(offset=offsets, energy_true=coords["energy_true"])
     return (exposure * livetime).to("m2 s")
 
-def make_acceptance(mask_safe, edisp, psf, model_pos, pointing, geom_model):
-    """Compute the acceptance cube with dimensions of `geom_model`. The cube can be multiplied to the model which is integrated on `geom_model` and the sum over the result will give the total number of model counts inside the `mask_safe` which is in reco coordinates.
+def make_acceptance(geom, mask, edisp, psf, model_pos, 
+                    pointing=None, max_radius=None, containment=0.999, factor=4):
+    """Compute the acceptance cube with dimensions of `geom`. 
+    The cube can be multiplied to the model which is integrated on `geom` 
+    and the sum over the result will give the total number of model counts inside 
+    the `mask_safe` which is in reco coordinates.
 
         Parameters
         ----------
-        mask_safe : `~gammapy.maps.WcsNDMap`
-            analysis mask in reco coordinates    
-        edisp : `~gammapy.irf.EnergyDispersion2D`
-            Energy Dispersion from the observaton
-        psf : `~gammapy.irf.PSF3D`
-            Point spread function from the observaton
-        model_pos : `~astropy.coordinates.SkyCoord`
-            Ceter position of the model. In the future this can be extracted from the geom_model
-        pointing : `~astropy.coordinates.SkyCoord`
-            Pointing position of the observation. Should be a single coordinates.
-        geom_model : `~gammapy.maps.WcsGeom`
+        geom: `~gammapy.maps.WcsGeom`
             Geometry with coordinates on which the model will be evaluated. Needs true energy axis and skycoord.
-
+        mask: `~gammapy.maps.WcsNDMap`
+            analysis mask in reco coordinates    
+        edisp: `~gammapy.irf.EnergyDispersion2D` or `~gammapy.irf.EDispMap` or `EDispKernelMap`
+            Energy Dispersion of the observaton
+        psf: `~gammapy.irf.PSF3D` or `~gammapy.irf.PSFMap`
+            Point spread function of the observaton
+        model_pos: `~astropy.coordinates.SkyCoord`
+            Ceter position of the model. (In the future this can be extracted from the geom_model)
+        pointing: `~astropy.coordinates.SkyCoord`
+            Pointing position of the observation. Should be a single coordinates. Only needed if 
+            the IRFs are not projected on the sky
+        max_radius : `~astropy.coordinates.Angle`
+            maximum angular size of the PSF kernel map
+        containment : float
+            Containment fraction to use as size of the PSF kernel. The max. radius
+            across all energies is used. The radius can be overwritten using
+            the `max_radius` argument.
+        factor : int
+            oversampling factor to compute the PSF
+        
         Returns
         -------
         acceptance : `~gammapy.maps.WcsNDMap`
             the acceptance map for the unbinned evaluator.
     """
-#     It will be model specific. 
-#     offset = offset of model_pos and pointing (same as is used for binned)
-#     geom_model = geom on which the model is evaluated, maybe we want to pass coords instead
-#     a new mask needs to be interpolated from the mask safe on the geom
-    offset = model_pos.separation(pointing)
-    ## get the new mask_safe based on the geometry on which the model is evaluated
-    geom_model_mask = geom_model.to_image().to_cube([mask_safe.geom.axes['energy']])
-    coords_model = geom_model_mask.get_coord()  # or take the coords directly if coords are passed
-    # the mask_safe in model geometry
+    ## get the energy axes and copy them for the transposed EDispKernel
+    e_reco = mask.geom.axes['energy']
+    e_true = geom.axes["energy_true"]
+    fake_reco = e_true.copy(name='energy')
+    fake_true = e_reco.copy(name='energy_true')
+    geom_model_mask = geom.to_image().to_cube([e_reco])
+    
+    ## the PSF needs a larger map for evaluation
+    if isinstance(psf, PSFMap) or isinstance(psf, PSF3D):
+        width_geom = geom.width.max()
+        if max_radius is None:
+            kwargs = {
+                "fraction": containment,
+                "energy_true": e_true.center,
+            }
+            if isinstance(psf, PSFMap):
+                kwargs['position'] = model_pos
+            elif isinstance(psf, PSF3D):
+                kwargs['offset'] = model_pos.separation(pointing)
+            radii = psf.containment_radius(**kwargs)
+            max_radius = np.max(radii)
+        geom_model_mask=geom_model_mask.to_odd_npix(max_radius + width_geom/2)
+        
+    ## interpolate the mask to the integration geometry on which the back folding takes place    
+    coords = geom_model_mask.get_coord()  
+    # the mask_safe in model geometry but with axis energy label energy_true
     mask_model = Map.from_geom(geom=geom_model_mask.as_energy_true, 
-                               data=mask_safe.interp_by_coord(coords_model))
+                               data=mask.interp_by_coord(coords))
     
     ## create edisp 
     # because we want to convolve a reco map we do a trick and create a transposed EdispKernel
-    edisp_kernel = edisp.to_edisp_kernel(offset, 
-                                         energy_true=geom_model.axes['energy_true'].edges , 
-                                         energy=mask_safe.geom.axes['energy'].edges)
-    # change the axes
-    ax_reco = geom_model.axes['energy_true'].copy()
-    ax_true = mask_safe.geom.axes['energy'].copy()
-    ax_reco._name="energy"
-    ax_true._name='energy_true'
-    edisp_kernelT = EDispKernel(axes=[ax_true, ax_reco], data = edisp_kernel.data.T)
-    
-    ## create the psf
-    # first a PSFMap with 3x3 pixels around the model_position
-    geom_psf = mask_safe.geom.to_image().cutout(model_pos, mask_safe.geom.pixel_scales*3)
-    geom_psf = geom_psf.to_cube([psf.axes['rad'], geom_model.axes['energy_true']])
-    psfmap = make_psf_map(psf, pointing, geom_psf)
-    # now get the kernel at the model position
-    psf_kernel = psfmap.get_psf_kernel(geom_model, position=model_pos)
+    if isinstance(edisp, EDispMap):
+        # we need to adapt to possibly different e_true axis
+        # so we we interpolate to different e_true before generating the kernel
+        migra = e_reco.edges[:,None]/e_true.center
+        coords = {
+            "skycoord": model_pos,
+            "energy_true": e_true.center,
+            "migra": migra,
+        }
+        values = edisp.edisp_map.integral(axis_name="migra", coords=coords)
 
-    acceptance=mask_model.apply_edisp(edisp_kernelT)  # order of edisp and psf doesn't matter much
-    return acceptance.convolve(psf_kernel) # but edisp does ereco --> etrue and psf is in etrue
+        axis = 0 # diff alon the migra (reco) axis which is at dim0 for coord[skycoord] beeing a single position
+        data = np.clip(np.diff(values.to_value(''), axis=axis), 0, np.inf)
+        edisp_kernelT = EDispKernel(axes=[fake_true, fake_reco], data = data)
+        
+    elif isinstance(edisp, EDispKernelMap):
+        # the reco energy axis should match, but this should be the case if the mask is in reco coords of the dataset
+        # different axis probably need new normalizationand this is not tested
+        assert e_reco == edisp.edisp_map.geom.axes["energy"]
+        coords = {
+            "skycoord": model_gauss.position,
+            "energy_true": e_true.center,
+            "energy": e_reco.center[:,None],
+        }
+        data = edisp.edisp_map.interp_by_coord(coords)  # interpolate to new e_true axis
+        edisp_kernelT = EDispKernel(axes=[fake_true, fake_reco], data = data)
+     
+    elif isinstance(edisp, EnergyDispersion2D):
+        offset = model_pos.separation(pointing)
+        edisp_kernel = edisp.to_edisp_kernel(offset, 
+                                             energy_true=e_true.edges , 
+                                             energy=e_reco.edges)
+        
+        edisp_kernelT = EDispKernel(axes=[fake_true, fake_reco], data = edisp_kernel.data.T)
+        
+    else: edisp_kernelT = None
+  
+    ## create the psf
+    if isinstance(psf, PSFMap):
+        # need to interpolate to the e_true of the geom
+        model_pos = psf._get_nearest_valid_position(model_pos)
+
+        geom_psf = geom.to_odd_npix(max_radius=max_radius)
+        geom_upsampled = geom_psf.upsample(factor=factor)
+        coords = geom_upsampled.get_coord(sparse=True)
+        rad = coords.skycoord.separation(geom.center_skydir)
+
+        coords = {
+        "energy_true": coords["energy_true"],
+        "rad": rad,
+        "skycoord": model_pos,
+        }
+
+        data = psf.psf_map.interp_by_coord(
+        coords=coords,
+        method="linear",
+        )
+
+        kernel_map = Map.from_geom(geom=geom_upsampled, data=np.clip(data, 0, np.inf))
+        kernel_map = kernel_map.downsample(factor, preserve_counts=True)
+        psf_kernel = PSFKernel(kernel_map, normalize=True)
+        
+    elif isinstance(psf, PSF3D):
+        # first a PSFMap with 3x3 pixels around the model_position
+        geom_psf = mask.geom.to_image().cutout(model_pos, mask.geom.pixel_scales*3)
+        geom_psf = geom_psf.to_cube([psf.axes['rad'], geom.axes['energy_true']])
+        psfmap = make_psf_map(psf, pointing, geom_psf)
+        # now get the kernel at the model position
+        psf_kernel = psfmap.get_psf_kernel(geom, position=model_pos)
+        
+    else: psf_kernel = None
+    
+    acceptance=mask_model.apply_edisp(edisp_kernelT)  # First apply the edisp to go from reco energy to true energy
+    if psf_kernel:
+        acceptance=acceptance.convolve(psf_kernel)
+        acceptance=acceptance.cutout(geom.center_skydir, width_geom)
+    return acceptance
